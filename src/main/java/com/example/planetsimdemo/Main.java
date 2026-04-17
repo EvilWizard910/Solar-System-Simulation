b4d38bd14dd13608dd790b604567a206261c5f47
aidan <Aidanj711@aol.com>
2026-04-16 21:55:50 -0400

    added time slider and fine tuned some controls, clean up some code as well

--- src/main/java/com/example/planetsimdemo/Main.java
+++ src/main/java/com/example/planetsimdemo/Main.java
@@ -11,8 +11,23 @@ import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import java.util.Locale;
 
 public class Main extends Application {
+
+
+    private static String formatSimulationSpeed(double secondsPerSecond) {
+        if (secondsPerSecond < 60) {
+            return String.format(Locale.US, "%.1fx real time", secondsPerSecond);
+        }
+        if (secondsPerSecond < 3600) {
+            return String.format(Locale.US, "%.1f min/sec", secondsPerSecond / 60.0);
+        }
+        if (secondsPerSecond < 86400) {
+            return String.format(Locale.US, "%.1f hr/sec", secondsPerSecond / 3600.0);
+        }
+        return String.format(Locale.US, "%.2f days/sec", secondsPerSecond / 86400.0);
+    }
 
     @Override
     public void start(Stage stage) {
@@ -55,7 +70,9 @@ public class Main extends Application {
         boolean enableRotation = true;
 
         long[] lastTime = {0};
-        double simulationSpeed = 86400; // 86400 = 1 real second = 1 simulated day
+        final double minSimulationSpeed = 1.0;       // real time
+        final double maxSimulationSpeed = 604800.0;  // 1 week / second
+        final double[] simulationSpeed = {1.0};
 
 
         AnimationTimer timer = new AnimationTimer() {
@@ -69,20 +86,10 @@ public class Main extends Application {
                 double dt = (now - lastTime[0]) / 1_000_000_000.0;
                 lastTime[0] = now;
 
-                solarSystem.updatePhysics(dt * simulationSpeed);
+                solarSystem.updatePhysics(dt * simulationSpeed[0]);
             }
         };
 
-        /* AnimationTimer timer = new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                solarSystem.update();
-
-                if (enableRotation) {
-                    spin.setAngle(spin.getAngle() + 0.1);
-                }
-            }
-        };*/
 
         timer.start();
 
@@ -97,17 +104,30 @@ public class Main extends Application {
 
         Button startStopButton = new Button("⏹️");
 
-      //  Button scaleButton = new Button("Realistic Scale");
-       // Button viewScaleButton = new Button("Convenient Scale ");
-
+        //scale slider
         Label scaleLabel = new Label("Body Scale");
         Slider scaleSlider = new Slider(0, 1, 0); // 0 = realistic, 1 = convenient
         solarSystem.setViewScale(0);
-
         scaleSlider.setShowTickLabels(true);
         scaleSlider.setShowTickMarks(true);
         scaleSlider.setMajorTickUnit(0.5);
         scaleSlider.setBlockIncrement(0.1);
+
+        //dt slider
+        Label dtLabel = new Label("Simulation Speed");
+        Slider dtSlider = new Slider(0, 1, 0);
+        dtSlider.setShowTickLabels(true);
+        dtSlider.setShowTickMarks(true);
+        dtSlider.setMajorTickUnit(0.25);
+        dtSlider.setBlockIncrement(0.05);
+
+        dtSlider.valueProperty().addListener((obs, oldValue, newValue) -> {
+            double t = newValue.doubleValue();
+            double speed = minSimulationSpeed*Math.pow(maxSimulationSpeed/minSimulationSpeed, t);
+            simulationSpeed[0] = speed;
+            dtLabel.setText("Simulation Speed: "+formatSimulationSpeed(speed));
+        });
+
 
         final boolean[] isRunning = {true};
 
@@ -117,6 +137,7 @@ public class Main extends Application {
                 startStopButton.setText("▶️");
                 isRunning[0] = false;
             } else {
+                lastTime[0] = 0;
                 timer.start();
                 startStopButton.setText("⏹️");
                 isRunning[0] = true;
@@ -126,10 +147,13 @@ public class Main extends Application {
             solarSystem.setViewScale(newValue.doubleValue());
         });
 
-       // viewScaleButton.setOnAction(_ -> {solarSystem.viewScale();});
-       // scaleButton.setOnAction(_ -> {solarSystem.realisticScale();});
 
-        controlsBox.getChildren().addAll(startStopButton,scaleLabel, scaleSlider);
+        controlsBox.getChildren().addAll(
+                startStopButton,
+                scaleLabel,
+                scaleSlider,
+                dtLabel,
+                dtSlider);
 
 
         // place controls on the right side
@@ -143,6 +167,8 @@ public class Main extends Application {
                 // Zoom
                 case UP -> camera.setTranslateZ(camera.getTranslateZ() + 5);
                 case DOWN -> camera.setTranslateZ(camera.getTranslateZ() - 5);
+                case RIGHT -> camera.setTranslateZ(camera.getTranslateZ() + .1);
+                case LEFT -> camera.setTranslateZ(camera.getTranslateZ() - .1);
 
 //                // Pan
                case A -> camera.setTranslateX(camera.getTranslateX() - 20);
@@ -150,10 +176,10 @@ public class Main extends Application {
                case W -> camera.setTranslateY(camera.getTranslateY() - 20);
                case S -> camera.setTranslateY(camera.getTranslateY() + 20);
 
-                case J -> camera.setTranslateX(camera.getTranslateX() - 5);
-                case L -> camera.setTranslateX(camera.getTranslateX() + 5);
-                case I -> camera.setTranslateY(camera.getTranslateY() - 5);
-                case K -> camera.setTranslateY(camera.getTranslateY() + 5);
+                case J -> camera.setTranslateX(camera.getTranslateX() - .5);
+                case L -> camera.setTranslateX(camera.getTranslateX() + .5);
+                case I -> camera.setTranslateY(camera.getTranslateY() - .5);
+                case K -> camera.setTranslateY(camera.getTranslateY() + .5);
 
 
                 // Rotate LEFT / RIGHT (Y axis)
