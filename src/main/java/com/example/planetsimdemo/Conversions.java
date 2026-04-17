d45476f53622d00c55bf6fb2687a5d0318de16cf
aidan <Aidanj711@aol.com>
2026-04-16 22:32:16 -0400

    added 4 galilean moons

--- src/main/java/com/example/planetsimdemo/Conversions.java
+++ src/main/java/com/example/planetsimdemo/Conversions.java
@@ -13,8 +13,12 @@ public class Conversions {
     public static final double Saturn_Mass = 568319000000000000000000000.0;
     public static final double Uranus_Mass = 86810300000000000000000000.0;
     public static final double Neptune_Mass = 102410000000000000000000000.0;
-    public static final double Moon_Mass = 73000000000000000000000.0;
-    public static final double Io_Mass = 89319379731108900000000.0;
+    //moons
+    public static final double Moon_Mass =      73000000000000000000000.0;
+    public static final double Io_Mass =        89319379731108900000000.0;
+    public static final double Europa_mass =    47998438387492700000000.0;
+    public static final double Ganymede_mass =  148000000000000000000000.0;
+    public static final double Callisto_mass =  108000000000000000000000.0;
 
     //using meters as pixels
     public static double metersToScene(double meters) {
